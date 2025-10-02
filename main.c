a282f9b9ba1b9e6c27b3f31cd77c2d3033967197
Raspibotics <48187235+raspibotics@users.noreply.github.com>
2025-10-02 08:27:49 +0000

    test

--- main.c
+++ main.c
@@ -2,6 +2,17 @@
 
 #include <stdio.h>
 
+
+typedef struct CircularBuffer {
+   int *head;
+   int *tail;
+   int len; 
+} CircularBuffer; 
+ 
+CircularBuffer *createBuffer(int len) { 
+
+}
+
 int main() {
    printf("Welcome back Your Name Here! We hope you enjoy the module :D\n"); // Welcome message thats a bit generic :)
    return 0; // return zero to indicate the program ran ok 
