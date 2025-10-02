2467320a6b897a6f0bff0106ed8453d92c7ece8b
Raspibotics <48187235+raspibotics@users.noreply.github.com>
2025-10-02 08:53:03 +0000

    two exclamations

--- main.c
+++ main.c
@@ -3,6 +3,6 @@
 #include <stdio.h>
 
 int main() {
-   printf("Welcome back Your Name Here! We hope you enjoy the module :D\n"); // Welcome message thats a bit generic :)
+   printf("Welcome back Your Name Here! We hope you enjoy the module!! :D\n"); // Welcome message thats a bit generic :)
    return 0; // return zero to indicate the program ran ok 
 }
